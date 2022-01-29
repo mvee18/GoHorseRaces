b8c7c3911d5e95abe85294556a33345fd20a075c
mvee <emvalenc@go.olemiss.edu>
2022-01-29 01:48:48 -0600

    added eco

--- economy/economy.go
+++ economy/economy.go
@@ -1,6 +1,13 @@
 package economy
 
+import "math/rand"
+
 type Money float64
+
+type OddsPool struct {
+	Wager     Money
+	Frequency float64
+}
 
 const takePercentage = 0.15
 
@@ -9,4 +16,11 @@ func CalculateOdds(bet Money, total Money) Money {
 	leftOver := total - bet
 
 	return leftOver / bet
+}
+
+func DistributeMoney(o []OddsPool) {
+	// Somewhere between 0-100000, but mostly around 50,000.
+	TotalWager := rand.NormFloat64() * 100000
+
+	
 }
